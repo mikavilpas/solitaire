046363fd7c66e123703cf5b4e43285c3f14f3a5e
Mika Vilpas <mika.vilpas@gmail.com>
2015-08-06 14:12:32 +0300

    Add instructions on how to connect to figwheel from emacs

--- README.md
+++ README.md
@@ -21,6 +21,21 @@ get a Browser Connected REPL. An easy way to try it is:
 
 and you should see an alert in the browser window.
 
+To get connect cider (in emacs) to this repl, do `cider-connect` to
+port 7888. This connects to figwheel's environment, a clojure repl. To
+connect to the cljs repl, see core.cljs. It does this, namely:
+
+```lisp
+(do
+  (use 'figwheel-sidecar.repl-api)
+  (cljs-repl))
+```
+
+You have to eval those in the repl buffer, not inline. It doesn't work
+for some reason.
+
+---
+
 To clean all compiled files:
 
     lein clean
@@ -34,6 +49,6 @@ get live reloading, nor a REPL.
 
 ## License
 
-Copyright © 2014 FIXME
+Copyright © 2015 FIXME
 
 Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
--- src/solitaire/core.cljs
+++ src/solitaire/core.cljs
@@ -1,6 +1,10 @@
 (ns ^:figwheel-always solitaire.core
-    (:require
-              [reagent.core :as reagent :refer [atom]]))
+    (:require [reagent.core :as reagent :refer [atom]]))
+
+(comment
+  (do
+    (use 'figwheel-sidecar.repl-api)
+    (cljs-repl)))
 
 (enable-console-print!)
 
@@ -8,7 +12,7 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(def app-state (atom {:text "Hello world!"}))
 
 (defn hello-world []
   [:h1 (:text @app-state)])
@@ -16,10 +20,9 @@
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
 
-
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
 
