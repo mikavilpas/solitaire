1bf7f27bed3e5cb691dd3e19e870fa6931ffa9c7
Mika Vilpas <mika.vilpas@gmail.com>
2015-08-11 18:07:09 +0300

    Start ui

--- src/solitaire/core.cljs
+++ src/solitaire/core.cljs
@@ -9,13 +9,35 @@
     (use 'figwheel-sidecar.repl-api)
     (cljs-repl)))
 
-(def app-state (atom {:text "Hello world!"}))
+;; I don't know solitaire terms so I just copy these from wikipedia
+(def app-state
+  (atom {:stock {}
+         :waste-heap {}
+         ;; A 2 3 4 5 6 7 8 9 10 J Q K
+         :foundations []
+         ;; K Q J 10 9 8 7 6 5 4 3 2 A
+         :tableau []}))
+
+(defn card-place []
+  [:div.card.card-place])
 
 (defn board []
-  [:div.board
-   [:h1 (:text @app-state)]])
+  [:div.container
+   [:h1 "Klondike Solitaire"]
+   [:div.container.board
+    [:div.row
+     ;; top row
+     [:div.col-xs-2 (card-place)]
+     [:div.col-xs-2.col-xs-offset-2 (card-place)]
+     [:div.col-xs-2 (card-place)]
+     [:div.col-xs-2 (card-place)]
+     [:div.col-xs-2 (card-place)]
+
+     ]
+    ]])
 
 (reagent/render-component [board]
-                          (. js/document (getElementById "app")))
+                          (js/document.getElementById "app"))
 
 (fw/start)
+
