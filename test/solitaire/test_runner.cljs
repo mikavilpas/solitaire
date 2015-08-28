9ec9d999bf1746806c4b9a3de4305aadecf37d98
Mika Vilpas <mika.vilpas@gmail.com>
2015-08-28 09:23:03 +0300

    Change test favicon based on test run outcome

--- test/solitaire/test_runner.cljs
+++ test/solitaire/test_runner.cljs
@@ -24,9 +24,16 @@
                                       "test-failed"))}
      (str "Test results: " (:success? @test-result))]]])
 
+(defn- show-favicon [success?]
+  (when-let [link-element (.getElementById js/document "my-favicon")]
+    (if success?
+      (set! (.-href link-element) "img/plus-1.png")
+      (set! (.-href link-element) "img/test-failure-favicon.png"))))
+
 (defmethod cljs.test/report [:cljs.test/default :end-run-tests] [m]
   (let [success? (cljs.test/successful? m)]
-    (swap! test-result assoc-in [:success?] success?)))
+    (swap! test-result assoc-in [:success?] success?)
+    (show-favicon success?)))
 
 
 (reagent/render-component [test-report]
