a85dfbc96a0a843a7769283d50da529566b1f9e2
Mika Vilpas <mika.vilpas@gmail.com>
2015-08-31 16:03:41 +0300

    Start implementing a colored overlay for selected cards

--- src/solitaire/view/actions.cljs
+++ src/solitaire/view/actions.cljs
@@ -1,15 +1,10 @@
 (ns solitaire.view.actions
   (:require [solitaire.logic :as l]))
 
-(defn select-or-move-card!
-  "card-id is e.g. ♥2, the card that was clicked"
-  [card-id app-state card-place-name]
-  (let [different-place-selected (and (:selected-place @app-state)
-                                      (not (= (:selected-place @app-state)
-                                              card-place-name)))]
-    (if different-place-selected
-      ;; (swap! app-state l/move-card card-id @app-state)
-      (swap! app-state assoc-in [:selected-place] card-id))))
+;; todo disallow selecting stock somehow
+(defn select-or-move! [app-state card-place-name]
+  (print "setting :selected-place to " card-place-name)
+  (swap! app-state assoc-in [:selected-place] card-place-name))
 
 (defn turn-card! [app-state card card-place-name]
   (swap! app-state l/turn-card card card-place-name))
--- src/solitaire/view/core.cljs
+++ src/solitaire/view/core.cljs
@@ -8,31 +8,38 @@
 
 (defonce app-state (atom (l/new-game-state)))
 
+(defn selectable [card-place-name]
+  {:on-click #(a/select-or-move! app-state card-place-name)
+   :class (when (= (:selected-place @app-state)
+                   card-place-name)
+            "selected")})
+
 (defn card
   [card-map card-place-name]
-  (let [id (:id card-map)]
-    (if (:facing-up card-map)
-      [:div.card-size.card-face
-       {:on-click #(a/select-or-move-card! id app-state card-place-name)
-        :class (when (:selected? card-map) "selected")}
-       (when (:facing-up card-map)
-         [:p.pull-left.card-content
-          {:class (if (#{:diamond :heart} (:suite card-map))
-                    "red"
-                    "black")}
-          id])]
-      [:div.card-size.facing-down
-       {:on-click #(a/turn-card! app-state card-map card-place-name)}])))
+  (if (:facing-up card-map)
+    [:div.card-size.card-face
+     [:div.selected-overlay
+      (selectable card-place-name)
+      (when (:facing-up card-map)
+        [:p.pull-left.card-content
+         {:class (if (#{:diamond :heart} (:suite card-map))
+                   "red"
+                   "black")}
+         (:id card-map)])]]
+    [:div.card-size.facing-down
+     {:on-click #(a/turn-card! app-state card-map card-place-name)}]))
 
 (defn card-place
-  ([]
-   [:div.card-place.card-size])
   ([app-state card-place-name]
    (if-let [c (first (get @app-state card-place-name))]
-     [:div.card-place (card c card-place-name)]
+     [:div.card-place
+      [:div.selected-overlay (selectable card-place-name)
+       (card c card-place-name)]]
      [:div.card-place.card-size
-      (when (= :stock card-place-name)
-        {:on-click #(a/reset-stock! app-state)})])))
+      [:div.selected-overlay
+       (merge (selectable card-place-name)
+              (when (= :stock card-place-name)
+                {:on-click #(a/reset-stock! app-state)}))]])))
 
 (defn board
   []
@@ -42,8 +49,8 @@
     [:div.row
      ;; top row
      [:div.col-xs-4
-      [:div.pull-left (card-place app-state :stock)]
-      [:div.col-xs-1 (card-place app-state :waste-heap)]]
+      [:div.pull-left.col-xs-5 (card-place app-state :stock)]
+      [:div.col-xs-6 (card-place app-state :waste-heap)]]
      [:div.col-xs-offset-1.col-xs-7.pull-right
       [:div.col-xs-3 (card-place app-state :foundation1)]
       [:div.col-xs-3 (card-place app-state :foundation2)]
