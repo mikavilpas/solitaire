aab52627a658a8dd407344ac102c59ca6787c8bd
Mika Vilpas <mika.vilpas@gmail.com>
2015-09-06 14:24:03 +0300

    Allow turning cards on tableau piles

--- src/solitaire/view/actions.cljs
+++ src/solitaire/view/actions.cljs
@@ -1,25 +1,29 @@
 (ns solitaire.view.actions
   (:require [solitaire.core.logic :as l]))
 
+(defn turn-card! [app-state card card-place-name]
+  (swap! app-state l/turn-card card card-place-name))
+
 (defn select-or-move! [app-state target-card-place]
-  (let [previous-selected-place (:selected-place @app-state)]
+  (let [target-card (last (get @app-state target-card-place))
+        previous-selected-place (:selected-place @app-state)]
     (cond
+      (not (:facing-up target-card))
+      (turn-card! app-state target-card target-card-place)
+
+      (or (= :stock target-card-place)
+          (empty? (get @app-state target-card-place)))
+      (swap! app-state assoc-in [:selected-place] nil)
+
       previous-selected-place
       (swap! app-state
              l/move-card-place-cards-to
              previous-selected-place
              target-card-place)
 
-      (or (= :stock target-card-place)
-          (empty? (get @app-state target-card-place)))
-      (swap! app-state assoc-in [:selected-place] nil)
-
       ;; valid card place?
       ((set l/card-places) target-card-place)
       (swap! app-state assoc-in [:selected-place] target-card-place))))
-
-(defn turn-card! [app-state card card-place-name]
-  (swap! app-state l/turn-card card card-place-name))
 
 (defn reset-stock! [app-state]
   (swap! app-state l/reset-stock))
