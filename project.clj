442831c67a93a4e4fe3c5f659f34d9fdbc0d5b6f
Mika Vilpas <mika.vilpas@gmail.com>
2015-09-22 12:05:48 +0300

    Fix fixwheel being unable to reload code
    It would print errors to the js console instead

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.122"]
+                 [org.clojure/clojurescript "0.0-3297"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [reagent "0.5.1"]]
+                 [reagent "0.5.0"]]
 
-  :plugins [[lein-cljsbuild "1.1.0"]
+  :plugins [[lein-cljsbuild "1.0.5"]
             [lein-figwheel "0.3.7"]]
 
   :source-paths ["src"]
